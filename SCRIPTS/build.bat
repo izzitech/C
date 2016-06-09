0deb9f494fcce3f470fbad00626fe0692e4c2d7a
Ivan <ivan@izzi.com.ar>
2016-06-09 14:03:11 -0300

    Improved build.bat

--- SCRIPTS/build.bat
+++ SCRIPTS/build.bat
@@ -1,1 +1,7 @@
-cl winMain.c /link ? /nologo
+@echo off 
+
+mkdir .\build
+pushd .\build
+pwd
+cl ..\winMain.c /nologo /wd4047 /wd4024 /wd4028 /wd4133
+popd
--- testing_menus/build.bat
+++ testing_menus/build.bat
@@ -3,5 +3,5 @@
 mkdir .\build
 pushd .\build
 pwd
-cl ..\winMain.c /nologo
+cl ..\winMain.c /nologo /wd4047 /wd4024 /wd4028 /wd4133
 popd
